93be0ad9d7ff2eb68a20a938d10ddb71f6a98dec
smfahim25 <smfahimhossen@gmail.com>
2022-05-19 00:46:05 +0600

    server added

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000
+const cors = require('cors');
+require('dotenv').config();
+//middleware
+app.use(express.json())
+app.use(cors())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ortxu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const taskCollection = client.db('todo').collection('task');
+
+        app.get('/task', async (req, res) => {
+            const query = {}
+            const cursor = taskCollection.find(query)
+            const tasks = await cursor.toArray()
+            res.send(tasks)
+        })
+        app.post('/task', async (req, res) => {
+            const newTask = req.body
+            const result = await taskCollection.insertOne(newTask)
+            res.send(result)
+        })
+        app.delete('/task/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await taskCollection.deleteOne(query)
+            res.send(result)
+        })
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})

6e28d9733051a48c5a978e6d5eaf81fc6bdd5e5a
smfahim25 <smfahimhossen@gmail.com>
2022-05-19 00:50:19 +0600

    server changed

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(cors())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ortxu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
 async function run() {
     try {
         await client.connect();

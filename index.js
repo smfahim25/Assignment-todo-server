7bc6d5db909ac33e06b6f790c4b76c2818af898d
smfahim25 <smfahimhossen@gmail.com>
2022-08-02 13:23:03 +0600

    file added

--- index.js
+++ index.js
@@ -7,18 +7,13 @@ require('dotenv').config();
 //middleware
 app.use(express.json())
 app.use(cors())
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ortxu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 async function run() {
     try {
         await client.connect();
         const taskCollection = client.db('todo').collection('task');
-
+        const commentCollection = client.db('todo').collection('comment');
 
         app.get('/task', async (req, res) => {
             const query = {}
@@ -29,6 +24,30 @@ async function run() {
         app.post('/task', async (req, res) => {
             const newTask = req.body
             const result = await taskCollection.insertOne(newTask)
+            res.send(result)
+        })
+        app.post('/addcomment', async (req, res) => {
+            const newComment = req.body
+            const result = await commentCollection.insertOne(newComment)
+            res.send(result)
+        })
+        app.get('/comment', async (req, res) => {
+            const query = {}
+            const cursor = commentCollection.find(query)
+            const comment = await cursor.toArray()
+            res.send(comment)
+        })
+        app.put('/task/:id', async (req, res) => {
+            const id = req.params.id
+            // const complete = req.body
+            const filter = { _id: ObjectId(id) }
+            // const options = { upsert: true }
+            const updatedoc = {
+                $set: {
+                    isCompleted: true
+                }
+            }
+            const result = await taskCollection.updateOne(filter, updatedoc)
             res.send(result)
         })
         app.delete('/task/:id', async (req, res) => {
